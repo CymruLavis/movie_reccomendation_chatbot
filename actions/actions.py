c5ce942d96e88a7608f5291d4d4c46a76afc77ca
Ethan <elavis00@outlook.com>
2024-02-11 17:42:03 +0000

    figuring out the TMDB api

--- actions/actions.py
+++ actions/actions.py
@@ -22,7 +22,26 @@ class ActionMovieSearch(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         header = tracker.get_slot() # get all the slot values for the api filters
+        api_key = "bd19a230d2f58acc317c86f89d8c7b23"
+        api_access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDE5YTIzMGQyZjU4YWNjMzE3Yzg2Zjg5ZDhjN2IyMyIsInN1YiI6IjY1YzIzMWQ5MDkyOWY2MDE2MWU0YTc0OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-TUBWSOE5-yUi8I8P1t5NrnHXN9BcZy3RATvNY5G2QQ"
+        url = "https://api.themoviedb.org/3/discover/movie?"
+        "include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+        headers = {
+            "accept": "application/json",
+            "Authorization": api_access_token
+        }
+        slots = {"starring": "this guy",
+                 "director": "that guy",
+                 "genre": "comedy"}
+        url_with_filters = self.build_url(url, )
+
         # make the api call
         # format the result
         # return the information
-        return []
+        return ["title", "rating"]
+    
+    def build_url(url, arguments):
+        # loop through dict or list of all the slots that have been filled by the user
+        # if the slot has data then add the argument to the url
+        # return new url
+        return url
---
+++ actions/api_test.py
@@ -0,0 +1,33 @@
+import requests
+
+url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&without_genres=comedy"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDE5YTIzMGQyZjU4YWNjMzE3Yzg2Zjg5ZDhjN2IyMyIsInN1YiI6IjY1YzIzMWQ5MDkyOWY2MDE2MWU0YTc0OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-TUBWSOE5-yUi8I8P1t5NrnHXN9BcZy3RATvNY5G2QQ"
+}
+genres = {
+    "Action": 28,
+    "Adventure": 12,
+    "Animation": 16,
+    "Comedy": 35,
+    "Crime": 80,
+    "Documentary": 99,
+    "Drama": 18,
+    "Family": 10751,
+    "Fantasy": 14,
+    "History": 36,
+    "Horror": 27,
+    "Music": 10402,
+    "Mystery": 9648,
+    "Romance": 10749,
+    "Science Fiction": 878,
+    "TV Movie" : 10770,
+    "Thriller": 53,
+    "War": 10752,
+    "Western": 37
+}
+
+response = requests.get(url, headers=headers)
+
+print(type(response.text))
