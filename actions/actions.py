bf85cfc27ef2b3633c02e2f354f66404d60972a5
Ethan <elavis00@outlook.com>
2024-02-18 19:07:08 +0000

    got the api call working

--- actions/actions.py
+++ actions/actions.py
@@ -12,7 +12,34 @@ from rasa_sdk.events import SlotSet
 import requests
 import json
 
-
+api_access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDE5YTIzMGQyZjU4YWNjMzE3Yzg2Zjg5ZDhjN2IyMyIsInN1YiI6IjY1YzIzMWQ5MDkyOWY2MDE2MWU0YTc0OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-TUBWSOE5-yUi8I8P1t5NrnHXN9BcZy3RATvNY5G2QQ"
+url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+headers = {
+    "accept": "application/json",
+    "Authorization": api_access_token
+}
+genres = {
+    "ACTION": 28,
+    "ADVENTURE": 12,
+    "ANIMATION": 16,
+    "COMEDY": 35,
+    "CRIME": 80,
+    "DOCUMENTARY": 99,
+    "DRAMA": 18,
+    "FAMILY": 10751,
+    "FANTASY": 14,
+    "HISTORY": 36,
+    "HORROR": 27,
+    "MUSIC": 10402,
+    "MYSTERY": 9648,
+    "ROMANCE": 10749,
+    "SCIENCE FICTION": 878,
+    "TV MOVIE" : 10770,
+    "THRILLER": 53,
+    "WAR": 10752,
+    "WESTERN": 37
+}
+filters = ['with_cast', 'with_crew', 'with_genres']
 
 class ActionMovieSearch(Action):
     def name(self) -> Text:
@@ -20,69 +47,106 @@ class ActionMovieSearch(Action):
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        # api_key = "bd19a230d2f58acc317c86f89d8c7b23"
-        api_access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDE5YTIzMGQyZjU4YWNjMzE3Yzg2Zjg5ZDhjN2IyMyIsInN1YiI6IjY1YzIzMWQ5MDkyOWY2MDE2MWU0YTc0OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-TUBWSOE5-yUi8I8P1t5NrnHXN9BcZy3RATvNY5G2QQ"
-        url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
-        headers = {
-            "accept": "application/json",
-            "Authorization": api_access_token
-        }
-        slots = self.get_slots(tracker) #get slot values for api filters
-        
-        url_with_filters = self.build_url(url, slots)
-        
-        # make the api call
-        response = requests.get(url_with_filters, headers=headers)
-        
-        SlotSet("title", slots["title"])
-
-        # format the result
-        # return the information
+        slots = self.get_slots(tracker, genres) #get slot values for api filters
+        slots_with_ids = self.fill_id_slots(slots=slots) #fill slot values with ids
+        results = self.get_suggestions(slots_with_ids)
+        if results == "Empty":
+            pass
+            # use dispatcher to return something 
+        else:
+            suggestion = self.choose_suggestion(results)
 
 
-        return ["title", "rating"]
+        return [SlotSet("title", suggestion["title"]), 
+                SlotSet("aggregate_rating", suggestion["aggregate rating"]), 
+                SlotSet("starring", suggestion["starring"]),
+                SlotSet("director", suggestion["director"]),
+                SlotSet("genre", suggestion["genre"])]  # may need to utilize dispatcher for returning values
+    # https://www.youtube.com/watch?v=VcbfcsjBBIg
+
     
-    def build_url(url, arguments):
-        filters = {} #slot names mapped to filter names
-
-        # loop through dict or list of all the slots that have been filled by the user
-        # if the slot has data then add the argument to the url
-        # return new url
-        return url
-    
-    def get_slots(tracker): 
-        return {"starring": tracker.get_slot('starring'),
+    def get_slots(self, tracker, genres): 
+        return {"starring": tracker.get_slot('starring'),   #may need to have get_slot("text", None)
+                "starring_id": "",
                  "director": tracker.get_slot('director'),
-                 "genre": tracker.get_slot('genre'),
+                 "director_id": "",
+                 "genre": genres[tracker.get_slot('genre').upper()],
+                 "genre_id": "",
                  "title": tracker.get_slot('title'),
                  "rating": tracker.get_slot('aggregate_score')}
     
-    def format_resonse(response):
-        # will take response.Requests() object
-        # parse to json or dictionary
-        # organize results into dictionary of:
-        # result = [
-        #     {
-        #         "title": "name",
-        #         "rating": "num",
-        #         "starring": "person",
-        #         "director": "person",
-        #         "genre": "genre"
+    def fill_id_slots(self, slots):
+        for key in slots:
+            if slots[key] != "":
+                if key == "starring" or key == "director":
+                    id_key = key+"_id"
+                    slots[id_key] = self.get_person_id(slots[key])
+                elif key == "genre":
+                        id_key = key+"_id"
+                        slots[id_key] = genres[slots[key]]
+        return slots
+    
+    def get_suggestions(self, slots):
+        response = requests.get(self.build_url(slots), headers=headers)
+        data = response.json()['results']
+        if data:
+            results = []
+            for i in range(10):
+                movie_id = data[i]['id']
+                people = self.get_movie_credits(movie_id)
+                useful_data = {
+                    "title": data[i]['title'],
+                    "aggregate rating": data[i]['vote_average'],
+                    "genres": data[i]['genre_ids'],
+                    "starring": people[0],
+                    "director": people[1],
+                }
+                results.append(useful_data)
+            return results
+        else:
+            return "Empty"
 
-        #     },
-        #      {
-        #         "title": "name",
-        #         "rating": "num",
-        #         "starring": "person",
-        #         "director": "person",
-        #         "genre": "genre"
+    
+    def build_url(self, slots):
+        return f"https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres={slots['genre_id']}&with_cast={slots['starring_id']}&with_crew={slots['director_id']}"
 
-        #     },
-        # ]
+    def get_movie_credits(self, movie_id):
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}/credits?language=en-US"
+        response = requests.get(url, headers=headers)
+        starring = response.json()['cast'][0]['name']
+        crew = response.json()['crew']
+        director = ''
+        for member in crew:
+            if member['job'].lower() == 'director':
+                director = member['name']
+        return starring, director
+    
+    def get_person_id(self, person_name):
+        person_name_no_spaces = person_name.replace(" ", "%20")
+        url = f"https://api.themoviedb.org/3/search/person?query={person_name_no_spaces}&include_adult=false&language=en-US&page=1"
+        response = requests.get(url, headers=headers)
 
-        # return result
-        pass
+        return response.json()['results'][0]['id']
+    
 
-    def set_slots():
-        # sets the first value of results back to slots
-        pass
+    def choose_suggestion(self, results):
+        max_score = 0
+        max_ind=0
+        for i in range(len(results)):
+            if results[i]['aggregate rating'] > max_score:
+                max_score = results[i]['aggregate rating']
+                max_ind = i
+        return {
+            'title': results[max_ind]['title'],
+            'aggregate rating': results[max_ind]['aggregate rating'],
+            'genres': self.genre_id_to_str(results[max_ind]['genres']),
+            'starring': results[max_ind]['starring'],
+            'director': results[max_ind]['director']
+        }
+        
+    def genre_id_to_str(self, genre_list):
+        for i in range(len(genre_list)):
+            for key, val in genres.items():
+                if val == genre_list[i]:
+                    genre_list[i] = key
+        return genre_list
